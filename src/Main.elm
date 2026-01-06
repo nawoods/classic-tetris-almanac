c0551881a424d6daeeca149dc05e6f938ec9fd8f
Nick Woods <14417375+nawoods@users.noreply.github.com>
2026-01-06 13:55:50 -0800

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,13 @@
+module Main exposing (..)
+import Browser
+
+main : Program () Model Msg
+main =
+  Browser.application
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    , onUrlChange = UrlChanged
+    , onUrlRequest = LinkClicked
+    }
