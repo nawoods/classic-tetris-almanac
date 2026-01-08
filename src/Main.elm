6100776f7957efbdad5a1b3ae5202680fcd86f68
Nick Woods <14417375+nawoods@users.noreply.github.com>
2026-01-07 18:13:47 -0800

    Use elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,10 @@ import Browser.Navigation as Nav
 import Url
 import Html
 import Time
+
+import Element as E
+import Element.Font as Font
+
 import TwoPlayerMatch
 
 main : Program () Model Msg
@@ -25,11 +29,17 @@ main =
 -- model --
 -----------
 
-type alias Model = Bool
+type Model
+  = Matches (List TwoPlayerMatch.TwoPlayerMatch)
+  | Error String
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ _ _ =
-  ( True, Cmd.none )
+  case TwoPlayerMatch.sampleData of
+    Ok matches ->
+      ( Matches matches, Cmd.none )
+    Err errorMessage ->
+      ( Error errorMessage, Cmd.none )
 
 
 
@@ -79,35 +89,43 @@ view model =
 
 body : Model -> Html.Html Msg
 body model =
-  Html.div [] [
-    sampleDataTable TwoPlayerMatch.sampleData
-  ]
+  E.layout
+    []
+    <|  case model of
+          Matches matches ->
+            sampleDataTable matches
+          Error error ->
+            E.text error
 
+sampleDataTable : List TwoPlayerMatch.TwoPlayerMatch -> E.Element msg
+sampleDataTable matches =
+  E.column
+    [ E.spacing 5
+    , E.width E.fill
+    ]
+    ( List.map sampleDataRow matches )
 
-sampleDataTable : Result String (List TwoPlayerMatch.TwoPlayerMatch) -> Html.Html Msg
-sampleDataTable data =
-  case data of
-    Ok matches ->
-      Html.div []
-      [
-        Html.table [] (List.map sampleDataRow matches)
-      ]
-    Err errorMessage ->
-      Html.div [] [ Html.text errorMessage ]
-
-sampleDataRow : TwoPlayerMatch.TwoPlayerMatch -> Html.Html Msg
+sampleDataRow : TwoPlayerMatch.TwoPlayerMatch -> E.Element msg
 sampleDataRow match =
-  Html.tr []
-  [ Html.td [] [ Html.text match.player1Name ]
-  , Html.td [] [ Html.text ("(" ++ String.fromInt match.player1Score ++ "-" ++ String.fromInt match.player2Score ++ ")") ]
-  , Html.td [] [ Html.text match.player2Name ]
-  , Html.td [] [ Html.text match.event ]
-  , Html.td [] [ Html.text (Maybe.withDefault "" match.edition) ]
-  , Html.td [] [ Html.text (Maybe.withDefault "" match.round) ]
-  , Html.td [] [ Html.text (formattedDate match.time) ]
-  , Html.td [] [ Html.text match.restreamerOrLocation ]
-  , Html.td [] [ Html.text (TwoPlayerMatch.matchTypeToString match.matchType) ]
-  ]
+  E.row []
+    [ sampleDataCell 100 match.player1Name
+    , sampleDataCell 30 ("(" ++ String.fromInt match.player1Score ++ "-" ++ String.fromInt match.player2Score ++ ")")
+    , sampleDataCell 100 match.player2Name
+    , sampleDataCell 150 match.event
+    , sampleDataCell 150 (Maybe.withDefault "" match.edition)
+    , sampleDataCell 150 (Maybe.withDefault "" match.round)
+    , sampleDataCell 100 (formattedDate match.time)
+    , sampleDataCell 150 match.restreamerOrLocation
+    , sampleDataCell 30 (TwoPlayerMatch.matchTypeToString match.matchType)
+    ]
+
+sampleDataCell : Int -> String -> E.Element msg
+sampleDataCell width content =
+  E.el
+    [ E.width (E.px width) 
+    , Font.size 12
+    ]
+    ( E.text content )
       
 to3LetterMonth : Time.Month -> String 
 to3LetterMonth month =
